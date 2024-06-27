d4fd5de7581f569849263ac293b13f803fbb25ec
bhaktigondal <bhaktigondal290@gmail.com>
2024-06-27 17:29:31 +0530

    adding categories

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,73 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Home.css";
 import addIcon from "./add.png";
-import ToDoCard from '../../components/ToDoCard/ToDoCard';
+import ToDoCard from "../../components/ToDoCard/ToDoCard";
 // import { useState } from 'react';
-import toast,{Toaster} from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 
 function Home() {
-    // const todoList = [
-    //     "go to market",
-    //     "go to gym",
-    //     "buy groceries",
-    //     "shopping",
-    //     "go to market",
-    //     "go to gym",
-    //     "buy groceries",
-    //     "shopping",
-    //     "go to market",
-    //     "go to gym",
-    //     "buy groceries",
-    //     "shopping"
-    // ];
-    const [todoList , setTodoList]=useState([
-        
-    ])
-    const [newTask, setNewTask] = useState("");
-  
-    return (
-        <div>
-            <h1 className='app-title'>To Do Application 📝</h1>
-            <div className='to-do-list-container'>
-               {
-                todoList.map((todoItem, i) => <ToDoCard key={i} todoItem={todoItem} />)
-               }
-               {
-                todoList.length===0 ?
-                <p style={{textAlign:"center"}}>no task to added,please add new task</p>
-                :
-                null
-               }
-              
-            </div>
-            <div className='to-do-item-container'>
-                <input type='text' 
-                placeholder='Add New Task'
-                 className='add-input' 
-                 value={newTask} 
-                 onChange={(e) => setNewTask(e.target.value)}/>
-                 <img 
-                    src={addIcon} 
-                    className='add-icon' 
-                    alt="Add" 
-                    onClick={
-                        ()=>{
-                            if(  newTask==="")
-                              
-                             {
-                                toast.error("task cannot ne empty")
-                                return
-                            }
-                            setTodoList([...todoList, newTask]);
-                            setNewTask(""); 
-                            toast.success("task added succesfully")
-                        }
-                    }
-                />
-            </div>
-            <Toaster position='top-right'/>
-        </div>
-    );
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [category, setCategory] = useState("");
+
+  return (
+    <div>
+      <h1 className="app-title">To Do Application 📝</h1>
+      <div className="to-do-list-container">
+        {todoList.map((todoItem, i) => (
+          <ToDoCard key={i} todoItem={todoItem} />
+        ))}
+        {todoList.length === 0 ? (
+          <p style={{ textAlign: "center" }}>
+            no task added,please add new task
+          </p>
+        ) : null}
+      </div>
+      <div className="to-do-item-container">
+        <input
+          type="text"
+          placeholder="Add New Task"
+          className="add-input"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+
+        <select className="category-select">
+          <option>Category</option>
+          <option>Learning</option>
+          <option>Work</option>
+          <option>Personal</option>
+          <option>Shopping</option>
+          <option>Health</option>
+          <option>Others</option>
+        </select>
+
+        <img
+          src={addIcon}
+          className="add-icon"
+          alt="Add"
+          onClick={() => {
+            if (newTask === "") {
+              toast.error("task cannot ne empty");
+              return;
+            }
+            setTodoList([...todoList, newTask]);
+            setNewTask("");
+            toast.success("task added succesfully");
+          }}
+        />
+      </div>
+      <Toaster position="top-right" />
+    </div>
+  );
 }
 
 export default Home;
